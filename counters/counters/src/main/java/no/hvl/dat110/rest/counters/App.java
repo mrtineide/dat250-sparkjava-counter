b0a7752b34804dd87ed004264152eaa6f0b62652
Martin Eide <oenesmartin@msn.com>
2021-09-28 13:15:01 +0200

    Expass1 part 2 done
    Implementet a GET, POST, PUT and DELETE HTTP request hanlder, with no error handling. This codes assumes that you know how to properly format your HTTP request for this server/ REST API.

--- counters/counters/src/main/java/no/hvl/dat110/rest/counters/App.java
+++ counters/counters/src/main/java/no/hvl/dat110/rest/counters/App.java
@@ -4,8 +4,13 @@ import static spark.Spark.after;
 import static spark.Spark.get;
 import static spark.Spark.port;
 import static spark.Spark.put;
+import static spark.Spark.post;
+import static spark.Spark.delete;
 
 import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Hello world!
@@ -14,6 +19,7 @@ import com.google.gson.Gson;
 public class App {
 	
 	static Counters counters = null;
+	static List<Todo> todos = null;
 	
 	public static void main(String[] args) {
 
@@ -24,6 +30,8 @@ public class App {
 		}
 
 		counters = new Counters();
+		todos = new ArrayList<>();
+
 		
 		after((req, res) -> {
   		  res.type("application/json");
@@ -37,7 +45,7 @@ public class App {
 
         get("/counters/green", (req, res) -> counters.getGreen());
 
-        // TODO: put for green/red and in JSON
+        // put for green/red and in JSON
         // variant that returns link/references to red and green counter
         put("/counters", (req,res) -> {
         
@@ -48,6 +56,72 @@ public class App {
             return counters.toJson();
         	
         });
+
+		get("/todo",(request,response)->{
+
+			Gson gson = new Gson();
+
+			String jsonString = gson.toJson(todos);
+
+			return jsonString;
+		});
+
+		get("/todo/*",(request,response)->{
+
+			Gson gson = new Gson();
+
+			int index = Integer.parseInt(request.splat()[0])-1;
+
+			Todo todo = todos.get(index);
+
+			String jsonString = gson.toJson(todo);
+
+			return jsonString;
+		});
+
+		post("/todo",(request,response)->{
+
+			Gson gson = new Gson();
+
+			Todo todo = gson.fromJson(request.body(),Todo.class);
+
+
+			todos.add(todo);
+
+			return gson.toJson(todo);
+		});
+
+		put("/todo/*",(request,response)->{
+
+			Gson gson = new Gson();
+
+			int index = Integer.parseInt(request.splat()[0])-1;
+
+			Todo todo = todos.get(index);
+
+			Todo new_todo = gson.fromJson(request.body(), Todo.class);
+
+			todo.setDescription(new_todo.getDescription());
+			todo.setSummary(new_todo.getSummary());
+
+			return gson.toJson(todo);
+		});
+
+		delete("/todo/*",(request,response)->{
+
+			Gson gson = new Gson();
+
+			String[] splats = request.splat();
+
+			Integer n = Integer.parseInt(splats[0]);
+
+			todos.remove(n-1);
+
+
+			return gson.toJson(todos);
+		});
+
+
     }
-    
+
 }
